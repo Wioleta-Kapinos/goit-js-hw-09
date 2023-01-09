59947bf4a74dea4f59f1a8d8c7a98d4d5df78aef
Wioleta-Kapinos <109916443+Wioleta-Kapinos@users.noreply.github.com>
2023-01-09 08:52:10 +0000

    Deploying to gh-pages from @ Wioleta-Kapinos/goit-js-hw-09@540dba6852b32ab547dd1e6db28becb79c1f114f 🚀

---
+++ 01-color-switcher.ae132685.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.disabled=!0,e.disabled=!1}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.ae132685.js.map
